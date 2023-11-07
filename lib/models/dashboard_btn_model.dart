a094d7c284fee00e6e5d699986c0a64d3a778d12
ahmed <ahmed.elsaba277@gmail.com>
2023-11-07 09:46:12 +0200

    first commit

---
+++ lib/models/dashboard_btn_model.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:go_shop_admin/screens/edit_or_upload_product_screen.dart';
+
+import '../screens/inner_screens/orders/orders_screen.dart';
+import '../screens/search_screen.dart';
+import '../services/assets_manager.dart';
+
+class DashboardButtonsModel {
+  final String text, imagePath;
+  final Function onPressed;
+
+  DashboardButtonsModel({
+    required this.text,
+    required this.imagePath,
+    required this.onPressed,
+  });
+
+  static List<DashboardButtonsModel> dashboardBtnList(BuildContext context) => [
+        DashboardButtonsModel(
+          text: "Add a new product",
+          imagePath: AssetsManager.cloud,
+          onPressed: () {
+            Navigator.pushNamed(context, EditOrUploadProductScreen.routeName);
+          },
+        ),
+        DashboardButtonsModel(
+          text: "inspect all products",
+          imagePath: AssetsManager.shoppingCart,
+          onPressed: () {
+            Navigator.pushNamed(context, SearchScreen.routeName);
+          },
+        ),
+        DashboardButtonsModel(
+          text: "View Orders",
+          imagePath: AssetsManager.order,
+          onPressed: () {
+            Navigator.pushNamed(context, OrdersScreenFree.routeName);
+          },
+        ),
+      ];
+}
